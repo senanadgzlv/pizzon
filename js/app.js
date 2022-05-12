dc85adde034a67660b2b070cfc73f4eff17fa3a8
Senan <senanadigozelov555@gmail.com>
2022-05-12 17:58:36 +0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,246 @@
+
+//BUTTON SELECTORLAR
+const drink=document.querySelector('.drinkbutton');
+const allButton=document.querySelector('.allbutton');
+const salad=document.querySelector('.saladbutton');
+const pasta=document.querySelector('.pastabutton');
+const burger=document.querySelector('.burgerbutton');
+const desert=document.querySelector('.desertbutton');
+const pizzaButton=document.querySelector('.pizzabutton');
+//Contentler foto
+const marg=document.querySelector('.marg');
+const soda=document.querySelector(".soda");
+const senan=document.querySelector(".senan");
+const cheaserSalad=document.querySelector(".cheaser");
+const seaFood=document.querySelector(".seafood");
+const cookie=document.querySelector(".cookie");
+const peperoniPizza=document.querySelector('.peperoni');
+const bismak=document.querySelector(".bismarck");
+
+//Content yazilar 
+const margYazi=document.querySelector(".margyazi");
+const link=document.querySelector('#link');
+const yazi=document.querySelector("#yazi");
+const qiymet=document.querySelector("#price");
+const cheaserLink=document.querySelector("#cheaserlink");
+const cheaserYazi=document.querySelector("#cheaseryazi");
+const cheaserPrice=document.querySelector("#cheaserprice");
+const seaLink=document.querySelector("#sealink");
+const seaYazi=document.querySelector("#seayazi");
+const seaPrice=document.querySelector("#seaprice");
+const chocolateLink=document.querySelector("#chocolatelink");
+const chocolateYazi=document.querySelector("#chocolateyazi");
+const chocolatePrice=document.querySelector("#chocolateprice");
+const peperoniLink=document.querySelector("#peperronilink");
+const peperoniYazi=document.querySelector("#peperroniyazi");
+const peperoniPrice=document.querySelector("#peperroniprice");
+const bismakLink=document.querySelector("#bismaklink");
+const bismakYazi=document.querySelector("#bismakyazi");
+const bismakPrice=document.querySelector("#bismakprice");
+const valdostanaLink=document.querySelector("#valdostanalink");
+const valdostanaYazi=document.querySelector("#valdostanayazi");
+const valdostanaPrice=document.querySelector("#valdostanaprice");
+
+
+
+
+
+
+const changeFoto=()=>{
+    marg.src="img/menu-3.png"
+
+   margYazi.innerHTML="RUM WITH SODA";
+    soda.style.display="none";
+    senan.style.display="none"
+   cheaserSalad.style.display="none";
+   seaFood.style.display="none";
+   cookie.style.display="none";
+   peperoniPizza.style.display="none";
+   bismak.style.display="none";
+   link.style.display="none";
+   yazi.style.display="none";
+   qiymet.style.display="none";
+   cheaserLink.style.display="none";
+   cheaserYazi.style.display="none";
+   cheaserPrice.style.display="none";
+   seaLink.style.display="none";
+   seaYazi.style.display="none";
+   seaPrice.style.display="none";
+   chocolateLink.style.display="none";
+   chocolateYazi.style.display="none";
+   chocolatePrice.style.display="none";
+   peperoniLink.style.display="none";
+   peperoniYazi.style.display="none";
+   peperoniPrice.style.display="none";
+   bismakLink.style.display="none";
+   bismakYazi.style.display="none";
+   bismakPrice.style.display="none";
+   valdostanaLink.style.display="none";
+   valdostanaYazi.style.display="none";
+   valdostanaPrice.style.display="none";
+
+    
+}
+
+
+const saladChange=()=>{
+    marg.src='img/menu-7.png';
+
+    margYazi.innerHTML="CEASER SALAD";
+     soda.style.display="none";
+     senan.style.display="none"
+    cheaserSalad.style.display="none";
+    seaFood.style.display="none";
+    cookie.style.display="none";
+    peperoniPizza.style.display="none";
+    bismak.style.display="none";
+    link.style.display="none";
+    yazi.style.display="none";
+    qiymet.style.display="none";
+    cheaserLink.style.display="none";
+    cheaserYazi.style.display="none";
+    cheaserPrice.style.display="none";
+    seaLink.style.display="none";
+    seaYazi.style.display="none";
+    seaPrice.style.display="none";
+    chocolateLink.style.display="none";
+    chocolateYazi.style.display="none";
+    chocolatePrice.style.display="none";
+    peperoniLink.style.display="none";
+    peperoniYazi.style.display="none";
+    peperoniPrice.style.display="none";
+    bismakLink.style.display="none";
+    bismakYazi.style.display="none";
+    bismakPrice.style.display="none";
+    valdostanaLink.style.display="none";
+    valdostanaYazi.style.display="none";
+    valdostanaPrice.style.display="none";
+}
+
+const pastaChange=()=>{
+    marg.src='img/menu-8.png';
+  
+    margYazi.innerHTML="SEA FOOD PASTA";
+     soda.style.display="none";
+     senan.style.display="none"
+    cheaserSalad.style.display="none";
+    seaFood.style.display="none";
+    cookie.style.display="none";
+    peperoniPizza.style.display="none";
+    bismak.style.display="none";
+    link.style.display="none";
+    yazi.style.display="none";
+    qiymet.style.display="none";
+    cheaserLink.style.display="none";
+    cheaserYazi.style.display="none";
+    cheaserPrice.style.display="none";
+    seaLink.style.display="none";
+    seaYazi.style.display="none";
+    seaPrice.style.display="none";
+    chocolateLink.style.display="none";
+    chocolateYazi.style.display="none";
+    chocolatePrice.style.display="none";
+    peperoniLink.style.display="none";
+    peperoniYazi.style.display="none";
+    peperoniPrice.style.display="none";
+    bismakLink.style.display="none";
+    bismakYazi.style.display="none";
+    bismakPrice.style.display="none";
+    valdostanaLink.style.display="none";
+    valdostanaYazi.style.display="none";
+    valdostanaPrice.style.display="none";
+}
+
+const allPhoto=()=>{
+ marg.src="img/senan.png";
+ margYazi.innerHTML="MARGHARITA PIZZA";
+    soda.style.display="inline-block";
+    senan.style.display="inline-block"
+   cheaserSalad.style.display="inline-block";
+   seaFood.style.display="inline-block";
+
+
+
+
+   cookie.style.display="inline-block";
+   peperoniPizza.style.display="inline-block";
+   bismak.style.display="inline-block";
+   link.style.display="inline-block";
+   yazi.style.display="inline-block";
+   qiymet.style.display="inline-block";
+   cheaserLink.style.display="inline-block";
+   cheaserYazi.style.display="inline-block";
+   cheaserPrice.style.display="inline-block";
+   seaLink.style.display="inline-block";
+   seaYazi.style.display="inline-block";
+   seaPrice.style.display="inline-block";
+   chocolateLink.style.display="inline-block";
+   chocolateYazi.style.display="inline-block";
+   chocolatePrice.style.display="inline-block";
+   peperoniLink.style.display="inline-block";
+   peperoniYazi.style.display="inline-block";
+   peperoniPrice.style.display="inline-block";
+   bismakLink.style.display="inline-block";
+   bismakYazi.style.display="inline-block";
+   bismakPrice.style.display="inline-block";
+   valdostanaLink.style.display="inline-block";
+   valdostanaYazi.style.display="inline-block";
+   valdostanaPrice.style.display="inline-block";
+}
+allButton.addEventListener("click",allPhoto);
+pasta.addEventListener("click",pastaChange);
+salad.addEventListener("click",saladChange);
+drink.addEventListener("click",changeFoto);
+//Finish 
+
+// //Slider
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
+
+
+//   Carousel
+
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  autoplay:true,
+  autoplayTimeout:1000,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:3
+      }
+  }
+})
+
+
+
+
+
+
